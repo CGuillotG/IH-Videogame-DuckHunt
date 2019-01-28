bc45dfa1782127ab527994d35cda26263adbdb42
CGuillotG <c.guillot@outlook.com>
2019-01-28 10:03:23 -0600

    Added frames per image for sprites, and a different structure for it's variables

--- main.js
+++ main.js
@@ -1,45 +1,66 @@
-let canvas = document.getElementById('canvas')
-canvas.width = 400
-canvas.height = 400
-let ctx = canvas.getContext('2d')
-let fps = 30
+let canvas = document.getElementById("canvas");
+canvas.width = 400;
+canvas.height = 400;
+let ctx = canvas.getContext("2d");
+let fps = 30;
 
 class Horse {
-    constructor(){
-        this.x = 0
-        this.y = 0
-        this.w = 100
-        this.h = 100
-        this.image = new Image()
-        this.image.src = "http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png"
-        this.sx = 0
-        this.sy = 0
-        this.sc = 10
-        this.sr = 1 
-    }
-    animate(){
-        console.log(
-            `${this.image}, sx ${this.sx}, sx ${this.sy}, w ${this.w}, h ${this.h}, x ${this.x}, y ${this.y}, c ${this.c}, r ${this.r}}`
-          );
-        ctx.clearRect(0,0,canvas.width, canvas.height);
-        ctx.drawImage(this.image,this.w*this.sx,this.h*this.sy,this.w,this.h,this.x,this.y,this.w,this.h)
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.w = 100;
+    this.h = 100;
+    this.image = new Image();
+    this.sprite = {
+      src:
+        "http://www.williammalone.com/articles/create-html5-canvas-javascript-sprite-animation/downloads/sprite-animation-demo/images/coin-sprite-animation.png",
+      sx: 0,
+      sy: 0,
+      sc: 10,
+      sr: 0,
+      sf: 2,
+      scf:0
+    };
+  }
+  animate(sprite) {
+    // console.log(
+    //   `${this.image}, sx ${this.sx}, sx ${this.sy}, w ${this.w}, h ${
+    //     this.h
+    //   }, x ${this.x}, y ${this.y}, c ${this.c}, r ${this.r}}`
+    // );
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.image.src = sprite.src
+    ctx.drawImage(
+      this.image,
+      this.w * sprite.sx,
+      this.h * sprite.sy,
+      this.w,
+      this.h,
+      this.x,
+      this.y,
+      this.w,
+      this.h
+    );
 
-        this.sx++;
-        if(this.sx>(this.sc-1)){
-            this.sx=0;
-            this.sy++;
-        }
-        if(this.sy>(this.sr-1)){
-            this.sy=0;
-        }
-
+    if (sprite.scf === 0) {
+        sprite.sx++;
+      if (sprite.sx > sprite.sc - 1) {
+        sprite.sx = 0;
+        sprite.sy++;
+      }
+      if (sprite.sy > sprite.sr - 1) {
+        sprite.sy = 0;
+      }
     }
+    if(sprite.scf < sprite.sf-1) {sprite.scf++}
+    else {sprite.scf = 0}
+  }
 }
 
-let horse = new Horse()
+let horse = new Horse();
 
-function update(){
-    horse.animate()
+function update() {
+  horse.animate(horse.sprite);
 }
 
-setInterval(update, 1000/fps)
+setInterval(update, 1000 / fps);
