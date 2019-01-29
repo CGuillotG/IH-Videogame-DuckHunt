8d203d510067a662f29b461da5a626a9ad41492b
CGuillotG <c.guillot@outlook.com>
2019-01-29 12:10:21 -0600

    Added fonts, adjusted keys and added environment class

--- main.js
+++ main.js
@@ -4,26 +4,41 @@ canvas.height = 840;
 let ctx = canvas.getContext("2d");
 let fps = 30;
 let images = {
-  bg1: "./images/backgroundHD.png"
+  bg: "./images/backgroundHD.png",
+  env: "./images/environmentHD.png"
 };
 
-function Background() {
+function Environment() {
   this.x = 0;
   this.y = 0;
   this.w = canvas.width;
   this.h = canvas.height;
   this.image = new Image();
-  this.image.src = "./images/backgroundHD.png";
+  this.image.src = images.env;
   this.draw = function() {
-    // console.log(this.image + " - " + this.x + " - " + this.y + " - " + this.w + " - " + this.h)
-    ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
+    ctx.drawImage(this.image, this.x, this.y, canvas.width, canvas.height);
   };
-  //this.image.onload = this.draw.bind(this);
 }
-function Keyboard() {
+
+function KeyLayout() {
+  this.color = "white"
+  this.keywidth = 140
+  this.keyheight = 145
   this.draw = function() {
-    ctx.strokeStyle = "green";
-    ctx.strokeRect(0, 0, 120, 120);
+    ctx.strokeStyle = this.color;
+    for (i = 0; i < 12; i++) {
+      ctx.strokeRect(0 + i * this.keywidth, 0, this.keywidth, this.keyheight);
+    }
+    for (i = 0; i < 12; i++) {
+      ctx.strokeRect(this.keywidth*.5 + i * this.keywidth, this.keyheight, this.keywidth, this.keyheight);
+    }
+    for (i = 0; i < 11; i++) {
+      ctx.strokeRect(this.keywidth + i * this.keywidth, this.keyheight*2, this.keywidth, this.keyheight);
+    }
+    for (i = 0; i < 10; i++) {
+      ctx.strokeRect(this.keywidth*1.5 + i * this.keywidth, this.keyheight*3, this.keywidth, this.keyheight);
+    }
+    ctx.strokeRect(this.keywidth*2.5, this.keyheight*4, this.keywidth*8, this.keyheight);
   };
 }
 
@@ -81,8 +96,8 @@ class Dog {
 }
 
 // let dog = new Dog();
-// let background = new Background();
-let keyboard = new Keyboard();
+let environment = new Environment();
+let keylayout = new KeyLayout();
 
 function start() {
   setInterval(update, 1000 / fps);
@@ -90,50 +105,16 @@ function start() {
 
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  background.draw();
-  keyboard.draw();
+  environment.draw();
+  keylayout.draw();
   dog.animate(dog.sprite);
 }
 
-// keyboard.draw()
-// background.draw();
-ctx.strokeStyle = "darkslategrey";
-for (i=0; i<12; i++) {
-  ctx.strokeRect(0+(i*140), 0, 140, 140);
-}
-for (i=0; i<12; i++) {
-  ctx.strokeRect(70+(i*140), 140, 140, 140);
-}
-for (i=0; i<11; i++) {
-  ctx.strokeRect(140+(i*140), 280, 140, 140);
-}
-for (i=0; i<10; i++) {
-  ctx.strokeRect(210+(i*140), 420, 140, 140);
-}
-ctx.strokeRect(350, 560, 1120, 140);
-
-
-
-
-
-
-// for (i=0; i<13; i++) {
-//   ctx.strokeRect(0+(i*120), 0, 120, 120);
-// }
-// ctx.strokeRect(1560, 0, 240, 120);
-// ctx.strokeRect(0, 120, 180, 120);
-// for (i=0; i<12; i++) {
-//   ctx.strokeRect(180+(i*120), 120, 120, 120);
-// }
-// ctx.strokeRect(0, 240, 240, 120);
-// for (i=0; i<11; i++) {
-//   ctx.strokeRect(240+(i*120), 240, 120, 120);
-// }
-// ctx.strokeRect(0, 360, 300, 120);
-// for (i=0; i<10; i++) {
-//   ctx.strokeRect(300+(i*120), 360, 120, 120);
-// }
-// ctx.strokeRect(1500, 360, 300, 120);
-// ctx.strokeRect(600, 480, 600, 120);
+keylayout.draw()
+// ctx.clearRect(0, 0, canvas.width, canvas.height);
+let image = new Image()
+image.src = images.env
+ctx.drawImage(image, 0, 0)
+// environment.draw();
 
 //start()
