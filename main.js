c1dab2a6b761d80ceda38966587a7938ce6323a3
CGuillotG <c.guillot@outlook.com>
2019-01-31 16:02:16 -0600

    Added Round system and Fly Away

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ let ctx = canvas.getContext("2d");
 let interval;
 let score = 0;
 let bullets = 20;
-let round = 0
-let duckHit = [0,0,0,0,0,0,0,0,0,0]
-let duckHitIndex = 0
+let round = 0;
+let duckHit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let duckHitIndex = 0;
 let isFreezeActive = false;
 let isPaused = false;
 let coopEnabled = false;
@@ -24,7 +24,7 @@ let images = {
   env: "./images/environmentHD.png",
   crossh: "./images/crosshairHD.png",
   ui: "./images/scoreboardHD.png",
-  bul:"./images/bulletHD.png",
+  bul: "./images/bulletHD.png",
   dckscr: "./images/duckScoreHD.png"
 };
 
@@ -48,7 +48,7 @@ function KeyLayout() {
     ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", "{"],
     ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "-"]
   ];
-  this.draw = function(color = "white") {    
+  this.draw = function(color = "white") {
     ctx.strokeStyle = color;
     for (i = 0; i < 12; i++) {
       ctx.strokeRect(0 + i * this.keywidth, 0, this.keywidth, this.keyheight);
@@ -164,6 +164,7 @@ function Duck() {
   this.isFlying = true;
   this.isFalling = false;
   this.isSlowed = false;
+  this.isGone = false;
   this.an = Math.random() * 45;
   this.vx = this.v * Math.cos(this.an);
   this.vy = -this.v * Math.sin(this.an);
@@ -181,16 +182,23 @@ function Duck() {
       this.x += this.vx;
       this.y += this.vy;
     }
+    if (Math.floor((frames - this.iframe) / fps) > 15) {
+      this.isGone = true;
+      this.isFlying = false;
+      this.color = "blue";
+      this.vx = 0;
+      this.vy = vMult * -10;
+    }
   };
   this.gotShot = () => {
-    this.color = 'red'
+    this.color = "red";
     this.vx = 0;
     this.vy = vMult * 10;
     this.isFlying = false;
     this.isFalling = true;
     score++;
-    duckHit[duckHitIndex] = 1
-    duckHitIndex++
+    duckHit[duckHitIndex] = 1;
+    duckHitIndex++;
   };
   this.randomFly = () => {
     let cframe = frames - this.iframe;
@@ -231,35 +239,47 @@ function FreezeBeam(x, y) {
   this.freeze = () => {
     for (d in ducks) {
       let nr = this.r + ducks[d].w / 2;
-      let dist = Math.sqrt(Math.pow(this.x-ducks[d].x, 2) + Math.pow(this.y-ducks[d].y, 2));
-      if(dist <= nr && isFreezeActive) {
-        ducks[d].isSlowed = true
+      let dist = Math.sqrt(
+        Math.pow(this.x - ducks[d].x, 2) + Math.pow(this.y - ducks[d].y, 2)
+      );
+      if (dist <= nr && isFreezeActive) {
+        ducks[d].isSlowed = true;
       } else {
-        ducks[d].isSlowed = false
+        ducks[d].isSlowed = false;
       }
     }
   };
 }
 
-function ScoreBoard(){
+function ScoreBoard() {
   this.board = new Image();
   this.board.src = images.ui;
-  this.bullet = new Image()
-  this.bullet.src = images.bul
-  this.ducks = new Image()
-  this.ducks.src = images.dckscr
+  this.bullet = new Image();
+  this.bullet.src = images.bul;
+  this.ducks = new Image();
+  this.ducks.src = images.dckscr;
   this.draw = () => {
     ctx.drawImage(this.board, 0, 0, canvas.width, canvas.height);
     ctx.drawImage(this.bullet, 1200, 750, 20, 45);
     for (d in duckHit) {
-      let ox = 670
-      if(d > 5) {
-        ox = 690
+      let ox = 670;
+      if (d > 5) {
+        ox = 690;
       }
       if (duckHit[d] === 0) {
-        ctx.drawImage(this.ducks, 0, 0, 700, 700, ox+d*45, 758, 30, 30)
+        ctx.drawImage(this.ducks, 0, 0, 700, 700, ox + d * 45, 758, 30, 30);
       } else {
-        ctx.drawImage(this.ducks, 700+700*d, 0, 700, 700, ox+d*45, 758, 30, 30)
+        ctx.drawImage(
+          this.ducks,
+          700 + 700 * d,
+          0,
+          700,
+          700,
+          ox + d * 45,
+          758,
+          30,
+          30
+        );
       }
     }
     ctx.fillStyle = "rgb(255,255,255)";
@@ -267,36 +287,75 @@ function ScoreBoard(){
     ctx.textAlign = "left";
     ctx.textBaseline = "middle";
     ctx.fillStyle = "#3fbfff";
-    ctx.fillText(`x${bullets.toString().padStart(2,'0')}`, 1225, 773)    
+    ctx.fillText(`x${bullets.toString().padStart(2, "0")}`, 1225, 773);
     ctx.fillStyle = "rgb(255,255,255)";
     ctx.font = '45px "Duck Hunt"';
-    ctx.fillText(score.toString().padStart(6,'0'), 1365, 775)
-    ctx.fillText(`ROUND ${round}`, 230, 775)
+    ctx.fillText(score.toString().padStart(6, "0"), 1365, 775);
+    ctx.fillText(`ROUND ${round}`, 230, 775);
     ctx.fillStyle = "#83d313";
-    ctx.fillText(`DUCKS`, 510, 775)
+    ctx.fillText(`DUCKS`, 510, 775);
     ctx.fillStyle = "#83d313";
-    ctx.fillText(`|`, 935, 773)
-  }
+    ctx.fillText(`|`, 935, 773);
+  };
 }
 
-environment = new Environment();
-keylayout = new KeyLayout();
-obstacles = [];
-ducks = [];
-freeze = new FreezeBeam();
-scoreboard = new ScoreBoard();
-ducks.push(new Duck());
+function Round(n) {
+  this.iframe = frames;
+  this.cframe = 0;
+  this.n = n;
+  this.lduck = 0;
+  this.nduck = 40;
+  this.numducks = 0;
+  this.roundended = false;
+  this.fframe;
+  this.refresh = () => {
+    if (this.roundended) {
+      if (ducks.length === 0) {
+        this.fframe = frames;
+      }
+      if (frames - this.fframe >= 40) {
+        if (duckHit.reduce((a, b) => a + b, 0) < 6) {
+          gameOver();
+        }
+      }
+    } else {
+      let random = Math.floor(1 + Math.random() * 5);
+      this.cframe = frames - this.iframe;
+      if (this.cframe === this.nduck) {
+        ducks.push(new Duck());
+        this.numducks++;
+        this.lduck = this.cframe;
+        this.nduck = this.cframe + random * fps;
+      }
+      if (this.numducks >= 10) {
+        this.roundended = true;
+      }
+    }
+  };
+}
+
+let roundInst = new Round(0);
+let environment = new Environment();
+let keylayout = new KeyLayout();
+let obstacles = [];
+let ducks = [];
+let freeze = new FreezeBeam();
+let scoreboard = new ScoreBoard();
 
 function start() {
-  resetVariables()
-  ducks.push(new Duck());
-  createObstacles("rgb(255,255,255,0.5)")
+  resetVariables();
+  createObstacles("rgb(255,255,255,0.5)");
   interval = setInterval(update, 1000 / fps);
 }
 
 function pause() {
   isPaused = !isPaused;
   drawPause();
+}
+
+function gameOver() {
+  isPaused = true;
+  //print Something
 }
 
 function update() {
@@ -310,25 +369,19 @@ function update() {
     keylayout.draw("rgb(255,255,255,0.5)");
   }
   for (o in obstacles) {
-    // obstacles[o].draw()
     obstacles[o].bounce(ducks);
   }
-  if (frames === 40) {
-    ducks.push(new Duck());
-  }
-  if (frames === 60) {
-    ducks.push(new Duck());
-  }
-  removeDuck()
+  roundInst.refresh();
+  removeDuck();
   for (d in ducks) {
     ducks[d].move();
     ducks[d].drawSquare();
     ducks[d].randomFly();
   }
   freeze.draw();
-  freeze.freeze()
+  freeze.freeze();
   environment.draw();
-  scoreboard.draw()
+  scoreboard.draw();
   frames++;
 }
 
@@ -342,16 +395,17 @@ function drawPause() {
   ctx.fillText("Paused", canvas.width / 2, canvas.height / 2);
 }
 
-function resetVariables(){
-  clearInterval(interval)
+function resetVariables() {
+  clearInterval(interval);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  isFreezeActive = false
-  round = 0
+  isFreezeActive = false;
+  round = 0;
   bullets = 20;
   score = 0;
-  frames = 0  
-  duckHit = [0,0,0,0,0,0,0,0,0,0]  
-  duckHitIndex = 0
+  frames = 0;
+  duckHit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  duckHitIndex = 0;
+  roundInst = new Round(0);
   environment = new Environment();
   keylayout = new KeyLayout();
   obstacles = [];
@@ -510,21 +564,40 @@ function registerShot(e) {
         ducks[d].y <= y + keyheight
       ) {
         ducks[d].gotShot();
-
       }
     }
   }
 }
 
-function removeDuck(){
+function removeDuck() {
   for (d in ducks) {
     if (!ducks[d].isFlying) {
       if (ducks[d].y > canvas.height || ducks[d].y < 0) {
-        ducks.splice(d, 1)
-        break
+        ducks.splice(d, 1);
+        break;
       }
     }
   }
+}
+
+function shuffleArray(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 addEventListener("keypress", e => {
@@ -537,12 +610,12 @@ addEventListener("keypress", e => {
   if (!isPaused) {
     registerShot(e.code);
     //cheatcode
-    console.log(e.code)
-    if (e.code === 'Backquote') {
-      let rand = Math.floor(Math.random()*ducks.length)
-      if(ducks[rand].isFlying){
-        ducks[rand].gotShot()
-        bullets--
+    console.log(e.code);
+    if (e.code === "Backquote") {
+      let rand = Math.floor(Math.random() * ducks.length);
+      if (ducks[rand].isFlying) {
+        ducks[rand].gotShot();
+        bullets--;
       }
     }
   }
@@ -551,25 +624,25 @@ addEventListener("keypress", e => {
 button1.addEventListener("click", function() {
   hidekeyboard = !hidekeyboard;
   if (button1.innerText === "Normal Mode") {
-    button1.innerText = "Expert Mode";      
-    button1.classList.add("pushed")
+    button1.innerText = "Expert Mode";
+    button1.classList.add("pushed");
   } else {
-    button1.innerText = "Normal Mode";     
-    button1.classList.remove("pushed")
-  }  
-  button1.blur()
+    button1.innerText = "Normal Mode";
+    button1.classList.remove("pushed");
+  }
+  button1.blur();
 });
 
 button2.addEventListener("click", function() {
   coopEnabled = !coopEnabled;
   if (button2.innerText === "Co-Op Disabled") {
-    button2.innerText = "Co-Op Enabled";    
-    button2.classList.add("pushed")
+    button2.innerText = "Co-Op Enabled";
+    button2.classList.add("pushed");
   } else {
     button2.innerText = "Co-Op Disabled";
-    button2.classList.remove("pushed")
+    button2.classList.remove("pushed");
   }
-  button2.blur()
+  button2.blur();
 });
 
 canvas.addEventListener("mousedown", function(e) {
@@ -581,10 +654,10 @@ canvas.addEventListener("mousedown", function(e) {
 
 button3.addEventListener("click", function() {
   if (isPaused) {
-    isPaused = false
+    isPaused = false;
   }
   start();
-  button3.blur()
-})
+  button3.blur();
+});
 
 start();
